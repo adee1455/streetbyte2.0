f7535293726507ceb72b55ed180826e9ec66c95f
adee1455 <shaikhadee1455@gmail.com>
2025-01-02 13:36:00 +0530

    manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,20 +15,37 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "StreetByte",
-  description: "Made with love by jadestudios",
+  description: "Made with love by Jade Studios",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/jade.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#EF4443" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              if ('serviceWorker' in navigator) {
+                window.addEventListener('load', () => {
+                  navigator.serviceWorker.register('/service-worker.js');
+                });
+              }
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header/>
+        <Header />
         {children}
       </body>
     </html>

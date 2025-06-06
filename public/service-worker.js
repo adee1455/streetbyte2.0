8c6cce0d6ed55a620bbf05c8bd86bcd4382e7a5b
adee1455 <shaikhadee1455@gmail.com>
2025-06-06 20:15:53 +0530

    App

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'streetbyte-v1';
+const CACHE_NAME = 'streetbyte-v2';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/logo.png',
+  '/ByteLogo.png',
   '/menu.avif',
   '/manifest.json'
 ];
@@ -11,6 +11,22 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+    .then(() => self.clients.claim())
   );
 });
 
